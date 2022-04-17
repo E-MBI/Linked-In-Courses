62221bf2f6a7a2a0a44cf0e83145fbfff8c7a387
Mahmoud Ebraheem <mahmoudibrahem101@outlook.com>
2022-04-17 06:50:23 +0200

    Update

---
+++ Programming Foundations/challThree.py
@@ -0,0 +1,12 @@
+def GetFavCity(name):
+    if name == "Elmadena Elmonwara":
+        print("One Of My Favorit city is ", name)
+    elif name == "Rooma":
+        print("The Second  Of My Favorit city is ", name)
+    elif name == "California":
+        print("The Third Of My Favorit city is ", name)
+
+
+GetFavCity("Elmadena Elmonwara")
+GetFavCity("Rooma")
+GetFavCity("California")
