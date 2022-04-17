eb349f68209894c55998d5c5da1e83388c0942ec
Mahmoud Ebraheem <mahmoudibrahem101@outlook.com>
2022-04-17 05:44:24 +0200

    Set_Up

---
+++ Programming Foundations/challOne.py
@@ -0,0 +1,12 @@
+# Challenge:1
+message = "this is going to be tricky;)"
+Message = "Very tricky"
+print(message)  # the out put will var number one as character case senstive
+
+# Performed Mathematical operations
+result = 2**3
+print("2**3=", result)
+result = 5-3
+print("5-3=", result)
+
+# complate challenge
---
+++ Programming Foundations/challTwo.py
@@ -0,0 +1,8 @@
+Guessing = input("What's my Favorite food ?")
+
+if Guessing == "meet":
+    print("Yep ! So Amazing")
+else:
+    print("Yuck ! That's Not It")
+
+print("Thank's For Playing")
