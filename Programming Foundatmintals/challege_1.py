aef9b1614628344fdb4699fad34c387fa1e6d113
Mahmoud Ebraheem <mahmoudibrahem101@outlook.com>
2022-04-18 19:26:20 +0200

    chall_3

---
+++ Programming Foundatmintals/challege_1.py
@@ -0,0 +1,24 @@
+# list  exmpale=>
+stars = [
+    'sol',
+    'Alpha Centauri',
+    'Barnard',
+    'Wolf 359'
+]
+print(stars[0])
+print(stars[1])
+print(stars[2])
+print(stars[3])
+# dictionary exmpale=>
+peaks = {
+    "African": "Kilimanjaro",
+    "Antarctic": "Vinson",
+    "Australian": "puncak Jaya",
+    "Eurasian": "Everset",
+    "North_American": "Denali",
+    "pacific": "Mauna kea",
+    "South_American": "Aconcagua",
+}
+print(peaks["African"])
+print(peaks["Antarctic"])
+print(peaks["Australian"])
