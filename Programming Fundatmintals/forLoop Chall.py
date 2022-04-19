dc2cd37474c13e580fb7e36ab18040899f892407
Mahmoud Ebraheem <mahmoudibrahem101@outlook.com>
2022-04-19 19:49:15 +0200

    for loop chall

---
+++ Programming Fundatmintals/forLoop Chall.py
@@ -0,0 +1,12 @@
+Fruits = [
+    "apples"
+    "bananas"
+    "dragon fruit"
+    "mangos"
+    "nectarines"
+    "pears"
+]
+# print this fruits in terminal
+print('My Favorite Fruits Is')
+for i in Fruits:
+    print(i)
